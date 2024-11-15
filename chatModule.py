95c09c254e00937bb4e5b585326e6b1bde7d17b6
Chang Sun <goodeffort2003@gmail.com>
2024-11-15 09:11:02 +0800

    Complete a simple UI of chatbot

--- chatModule.py
+++ chatModule.py
@@ -1,5 +1,37 @@
 from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+
+class ollamaGenerator:
+    model = ""
+    temperature = 0.2
+    memType = ""
+    systemMsg = ""
+    def __init__(self , model , temperature = 0.2 , systemMsg = ""):
+        self.model = model
+        self.temperature = temperature
+        self.systemMsg = systemMsg
+
+    def generateLLM(self):
+        llm = ChatOllama(
+            model = self.model,
+            temperature = self.temperature
+        )
+        return llm
+
+    def chatResponse(self , userMsg):
+        promptTemplate = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "You are a helpful assistant"
+                ),
+                (
+                    "human", "{inputMsg}"
+                ),
+        ])
+        llm = self.generateLLM()
+        chain = promptTemplate | llm
+        modelResponse = chain.invoke({"inputMsg": userMsg})
+        return modelResponse 
 
 
-def iniChatModel(modelName , temperature):
-    
--- chatUI.py
+++ chatUI.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from chatModule import ollamaGenerator as chatbot
+from langchain_ollama import ChatOllama
+
+ollamaGen = chatbot("smollm2:360m" , 0.5)
+chatMsgHistory = []
+if "chatMsgHistory" not in st.session_state:
+    st.session_state.chatMsgHistory = []
+
+for msgItem in st.session_state.chatMsgHistory:
+    with st.chat_message(msgItem["role"]):
+        st.markdown(msgItem["content"])
 
 with st.sidebar:
     uploadFile = st.file_uploader(
@@ -7,5 +18,20 @@ with st.sidebar:
     if uploadFile is not None:
         print(f"Filename is : {uploadFile.name}")
 
-with st.chat_message("user"):
-    st.write("Hello")
+userInput = st.chat_input("Please input message here...")
+
+for item in chatMsgHistory:
+    with st.chat_message(item["role"]):
+        st.write(item["content"])
+
+if userInput:
+    st.session_state.chatMsgHistory.append({"role": "user", "content": userInput} )
+    with st.chat_message("user"):
+        st.write(userInput)
+
+    modelRes = ollamaGen.chatResponse(userInput)
+    if modelRes.content:
+        st.session_state.chatMsgHistory.append({"role": "Assistant", "content": modelRes.content})
+        print(modelRes.content)
+        with st.chat_message("Assistant"):
+            st.markdown(modelRes.content)
