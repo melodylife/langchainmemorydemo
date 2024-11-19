d33010c70c57dde7c771a47aecea4b4d6a0ef8f9
Chang Sun <goodeffort2003@gmail.com>
2024-11-20 00:03:53 +0800

    Add stream mode and clean redundant code.

--- chatModule.py
+++ chatModule.py
@@ -16,13 +16,19 @@ class ollamaGenerator:
     def generateLLM(self , model):
         llm = ChatOllama(
             model = self.model,
-            temperature = self.temperature
+            temperature = self.temperature,
         )
         return llm
 
     def setModel(self , modelSel):
-        print(f"Model is set to {modelSel}")
+        #print(f"Model is set to {modelSel}")
         self.model = modelSel
+
+    def streamGenerator(self , userMsg , chatHistory , histAppr):
+        if histAppr == "Full memory history":
+            return self.chatResponse(userMsg , chatHistory)
+        elif histAppr == "Summary of History":
+            return self.chatResbySummary(userMsg , chatHistory)
 
     def summarizeHistory(self , chatHistory):
         msghistory = [("system" , "You are helpful assistant having expertise on summarizing chat history without missing any details.") , ("user" , "Following Chat History is a history of humane and AI assistant chat. Summarize the chat history into a single summary message. Include as many specific details as you can. Respond user only with the content of the summary but no other extra content generated.  Place the summarized content after keyword summary in the format of SUMMARY: . \n\n Chat History: {chathist}")]
@@ -35,33 +41,32 @@ class ollamaGenerator:
         chain = promptTemplate | llm
         summaryResponse = chain.invoke({"chathist" , histStr})
         print(summaryResponse.content)
-        #self.histSummary = summaryResponse.content
         return summaryResponse.content
 
-    def chatResbySummary(self , userMsg , chatHistory):
-        #msgTemplate = [("system", "You are a helpful assistant. Answer the question based on the summary shared along with the prompt.If the summary is empty, just answer the question regardless of it.")]
-        chatSummary = self.summarizeHistory(chatHistory)
+    def chatResbySummary(self , userMsg , msgHistory):
+        chatSummary = self.summarizeHistory(msgHistory)
         msgTemplate = ["system", "You are a helpful chatbot and answer questions based on the chat history provided in the context"]
         msgTemplate = msgTemplate + [("user" , "You are chatbot designed to answer user's questions:{questionMsg}\n\n Genrate your response exclusively from the provided chat history summary: {histSummary}. \n\nIf there's no helpful details in the summary, just say you don't know or ask the user to provide the context.")]
         promptTemplate = ChatPromptTemplate.from_messages(msgTemplate)
         llm = self.generateLLM(model = "gemma2:2b")
         chain = promptTemplate | llm
-        print("This is the summary "+ chatSummary)
+        #print("This is the summary "+ chatSummary)
         #print(promptTemplate.messages)
         set_verbose(True)
-        modelResponse = chain.invoke({"histSummary": chatSummary , "questionMsg": userMsg})
-        return modelResponse
+        return chain.stream({"histSummary": chatSummary , "questionMsg": userMsg})
 
     def chatResponse(self , userMsg , msgHistory):
+        chatHistory = []
+        for msgHisItem in msgHistory:
+            chatHistory.append((msgHisItem["role"], msgHisItem["content"]))
         msgTemplate = [("system" , "You are a helpful assistant. Answer the questions based on the history shared along with the prompt. Answer the questions according to the context in the history")]
-        if len(msgHistory) > 0:
-            msgTemplate = msgTemplate + msgHistory
+        if len(chatHistory) > 0:
+            msgTemplate = msgTemplate + chatHistory
         msgTemplate = msgTemplate + [("user" , "{inputMsg}")]
         #print(msgTemplate)
         promptTemplate = ChatPromptTemplate.from_messages(msgTemplate)
         llm = self.generateLLM(model = "gemma2:2b")
         chain = promptTemplate | llm
-        modelResponse = chain.invoke({"inputMsg": userMsg})
-        return modelResponse
+        return chain.stream({"inputMsg": userMsg})
 
 
--- chatUI.py
+++ chatUI.py
@@ -12,6 +12,14 @@ if "inputToken" not in st.session_state:
 if "chatmodel" not in st.session_state:
     st.session_state.chatmodel = ""
 
+def streamWrapper(streamLLM):
+    aggregate = None
+    for chunk in streamLLM:
+        aggregate = chunk if aggregate is None else aggregate + chunk
+        yield(chunk)
+    st.session_state.inputToken = aggregate.usage_metadata["input_tokens"]
+    return aggregate
+
 for msgItem in st.session_state.chatMsgHistory:
     with st.chat_message(msgItem["role"]):
         st.markdown(msgItem["content"])
@@ -22,7 +30,6 @@ with st.sidebar:
                  index = 0,
                  )
     ollamaGen.setModel(modelSel)
-    #st.session_state.chatmodel = modelSel
     tokenAmnt = st.info("" , icon = "👀")
     st.divider()
     memoFunction = st.radio(
@@ -43,21 +50,10 @@ if userInput:
     with st.chat_message("user"):
         st.write(userInput)
 
-    msgHistoryList = []
-    for msgHis in st.session_state.chatMsgHistory:
-        msgHistoryList.append((msgHis["role"] , msgHis["content"]))
-
     modelRes = None
-    if memoFunction == "Full memory history":
-        modelRes = ollamaGen.chatResponse(userInput , msgHistoryList)
-    elif memoFunction == "Summary of History":
-        modelRes = ollamaGen.chatResbySummary(userInput , st.session_state.chatMsgHistory)
-    if modelRes.content:
-        st.session_state.chatMsgHistory.append({"role": "ai", "content": modelRes.content})
-        msgHistoryList.append(("user" , modelRes.content))
-        #ollamaGen.summarizeHistory(st.session_state.chatMsgHistory)
-        with st.chat_message("ai"):
-            st.markdown(modelRes.content)
-            st.session_state.inputToken = modelRes.usage_metadata["input_tokens"]
-            tokenAmnt.text(f"Here is the total input token of the prompt:  {st.session_state.inputToken}")
-            #print(modelRes.usage_metadata)
+    streamOutput = ollamaGen.streamGenerator(userInput , st.session_state.chatMsgHistory , memoFunction)
+    with st.chat_message("ai"):
+        modelRes =  st.write_stream(streamWrapper(streamOutput))
+        tokenAmnt.text(f"Here is the total input token of the prompt:  {st.session_state.inputToken}")
+        st.session_state.chatMsgHistory.append({"role": "ai", "content": modelRes})
+
