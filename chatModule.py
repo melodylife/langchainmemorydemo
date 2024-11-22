7733faed6affde7e9ebe8b0d49dc13e95b73da9c
Chang Sun <goodeffort2003@gmail.com>
2024-11-22 17:51:18 +0800

    Unify the response into full output mode instead of stream mode

--- chatModule.py
+++ chatModule.py
@@ -52,13 +52,26 @@ class ollamaGenerator:
         #print(f"Model is set to {modelSel}")
         self.model = modelSel
 
+    def llmResponse(self , userMsg , chatHistory , histAppr):
+        llmRes = ""
+        if histAppr == "Full memory history":
+            llmRes = self.chatResponse(userMsg , chatHistory)
+        elif histAppr == "Summary of History":
+            llmRes = self.chatResbySummary(userMsg , chatHistory)
+        else:
+            #self.compileGraph()
+            config = {"configurable": {"thread_id": "1"}}
+            event = self.graph.stream({"messages": [("user" , userMsg)]} , config , stream_mode = "updates")
+            llmRes = event["chatbot"]["messages"].content
+        return llmRes
+            
     def streamGenerator(self , userMsg , chatHistory , histAppr):
         if histAppr == "Full memory history":
             return self.chatResponse(userMsg , chatHistory)
         elif histAppr == "Summary of History":
             return self.chatResbySummary(userMsg , chatHistory)
         else:
-            self.compileGraph()
+            #self.compileGraph()
             config = {"configurable": {"thread_id": "1"}}
             return self.graph.stream({"messages": [("user" , userMsg)]} , config , stream_mode = "values")
 
@@ -85,7 +98,8 @@ class ollamaGenerator:
         #print("This is the summary "+ chatSummary)
         #print(promptTemplate.messages)
         set_verbose(True)
-        return chain.stream({"histSummary": chatSummary , "questionMsg": userMsg})
+        #return chain.stream({"histSummary": chatSummary , "questionMsg": userMsg})
+        return chain.invoke({"histSummary": chatSummary , "questionMsg": userMsg})
 
     def chatResponse(self , userMsg , msgHistory):
         chatHistory = []
@@ -99,6 +113,7 @@ class ollamaGenerator:
         promptTemplate = ChatPromptTemplate.from_messages(msgTemplate)
         llm = self.generateLLM(model = "gemma2:2b")
         chain = promptTemplate | llm
-        return chain.stream({"inputMsg": userMsg})
+        #return chain.stream({"inputMsg": userMsg})
+        return chain.invoke({"inputMsg": userMsg})
 
 
